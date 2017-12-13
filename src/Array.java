a0e5737b5ef0c6e9647a37d975b2e5f44dc4318a
Muntaqim Elahi <MuntaquimE@gmail.com>
2017-12-12 23:25:31 -0500

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args){
+        String[] list = {"7","6","10","22","9","8","23","90","17","11"};
+        for (int i=0; i<list.length; i++){
+            System.out.println(list[i]);
+        }
+    }
+}
